5a1f89e8e3c208728435dac95d5b4d447d606a95
npezza93 <npezza93@gmail.com>
2017-07-31 22:06:16 -0400

    Add runner class to keep track of what slide you're on and handle commands

---
+++ lib/lecture/runner.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+module Lecture
+  class Runner
+    include Terminal
+
+    attr_accessor :current_slide, :slides, :deck
+
+    def initialize(deck)
+      @current_slide = 0
+      @slides        = []
+      @deck          = deck
+    end
+
+    def execute
+      load deck
+      first_slide
+
+      loop do
+        command_handler
+      end
+    end
+
+    private
+
+    def command_handler
+      case getch
+      when " ", "\e[C" then next_slide
+      when "\e[D"      then prev_slide
+      when "a"         then first_slide
+      when "d"         then last_slide
+      when "j"         then jump_to
+      when "q"         then clear_and_exit
+      end
+    end
+
+    def display(slide_number)
+      clear
+      self.current_slide = slide_number
+
+      clear_and_exit if slides.length - 1 < current_slide
+
+      slides.at(current_slide).display
+
+      move_to(0, lines)
+    end
+
+    def jump_to
+      move_to(0, lines)
+      print "Jump to: "
+
+      display(STDIN.gets.strip.to_i)
+    end
+
+    def next_slide
+      display(current_slide + 1)
+    end
+
+    def prev_slide
+      display(current_slide - 1)
+    end
+
+    def first_slide
+      display(0)
+    end
+
+    def last_slide
+      display(slides.size - 1)
+    end
+  end
+end
