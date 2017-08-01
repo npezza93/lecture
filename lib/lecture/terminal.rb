00cc24c4bf2ebf3dabdce8eed2101433d1d59900
npezza93 <npezza93@gmail.com>
2017-07-31 22:06:41 -0400

    Sort and add some helper terminal methods

--- lib/lecture/terminal.rb
+++ lib/lecture/terminal.rb
@@ -2,8 +2,30 @@
 
 module Lecture
   module Terminal
+    private
+
+    def clear
+      print "\e[2J\e[H"
+    end
+
+    def clear_and_exit(exit_status = 0)
+      clear
+      exit exit_status
+    end
+
     def cols
       IO.console.winsize[1]
+    end
+
+    def getch
+      prep_for_keyboard_entry do
+        if (input = STDIN.getc.chr) == "\e"
+          input << STDIN.read_nonblock(3) rescue nil
+          input << STDIN.read_nonblock(2) rescue nil
+        end
+
+        input
+      end
     end
 
     def lines
@@ -14,24 +36,27 @@ module Lecture
       print "\033[#{y};#{x}H"
     end
 
-    def clear
-      puts "\e[2J\e[H"
-    end
+    def prep_for_keyboard_entry
+      begin
+        STDIN.echo = false
+        STDIN.raw!
 
-    def available_colors
-      String.colors
-    end
+        input = yield
+      ensure
+        STDIN.echo = true
+        STDIN.cooked!
+      end
 
-    def available_modes
-      String.modes
+      input
     end
 
     def print_line(line, x, y)
       move_to(x, y)
+      tran_time = 0.3
 
       line.each_char do |c|
         print c
-        sleep CHARACTER_PRINT_DELAY
+        sleep [tran_time / line.length, Lecture.character_print_delay].min
       end
     end
   end
