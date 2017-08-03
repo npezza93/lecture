fe7904497d60d10e6d6598772c3097c095486738
npezza93 <npezza93@gmail.com>
2017-08-02 21:50:27 -0400

    Refactor code slide type to utilize the block slide type

--- lib/lecture/slide/code.rb
+++ lib/lecture/slide/code.rb
@@ -4,22 +4,10 @@ module Lecture
   class Slide
     class Code < Base
       def display
-        content.each_line.with_index do |line, i|
-          print_line(line, col, row + i)
-        end
+        Block.new(content: pygmentize_code(@content, lexer)).display
       end
 
       private
-
-      def col
-        [1, 1 + (cols - content.each_line.map do |line|
-          line.chomp.uncolorize.length
-        end.max) / 2].max
-      end
-
-      def content
-        pygmentize_code(@content, lexer)
-      end
 
       def pygmentize_code(code, lexer)
         Pygments.highlight(
@@ -27,10 +15,6 @@ module Lecture
           formatter: "terminal16m", lexer: lexer,
           options: { style: Lecture.pygment_style }
         ).gsub("\e[39m", "\e[0m")
-      end
-
-      def row
-        [1, 1 + (lines - content.count("\n")) / 2].max
       end
     end
   end
