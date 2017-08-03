f1dff18c31e78d0ce17a0b2c6d94ea26cab7152e
npezza93 <npezza93@gmail.com>
2017-08-02 21:44:41 -0400

    Require active support and slide factory class

--- lib/lecture.rb
+++ lib/lecture.rb
@@ -3,9 +3,11 @@
 require "pygments.rb"
 require "colorize"
 require "io/console"
+require "active_support/all"
 
 require "lecture/version"
 require "lecture/terminal"
+require "lecture/slide"
 
 module Lecture
   CHARACTER_PRINT_DELAY = 0.018
